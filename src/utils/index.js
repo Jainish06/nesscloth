e58e3cb5b1cf7726e63f2c515c99bd6784f6c913
Jainish06 <pathakjainish@gmail.com>
2024-05-19 00:28:48 +0530

    Small changes to ui.

---
+++ src/utils/index.js
@@ -0,0 +1,40 @@
+
+export const navOptions = [
+    {
+      id: "home",
+      label: "Home",
+      path: "/",
+    },
+    {
+      id: "listing",
+      label: "All Products",
+      path: "/product/listing/all-products",
+    },
+    {
+      id: "listingMen",
+      label: "Men",
+      path: "/product/listing/men",
+    },
+    {
+      id: "listingWomen",
+      label: "Women",
+      path: "/product/listing/women",
+    },
+  ];
+
+  export const adminNavOptions = [
+    {
+      id: "adminListing",
+      label: "Manage All Products",
+      path: "/admin-view/all-products",
+    },
+    {
+      id: "adminNewProduct",
+      label: "Add New Product",
+      path: "/admin-view/add-product",
+    },
+  ];
+
+  export const styles = {
+    button : 'bg-gray-900 hover:bg-gray-700 text-white font-medium py-2 px-5 rounded',
+  };
