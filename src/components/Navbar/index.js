bf64c72dff554edc1012d212ca91254aaf66849d
Jainish06 <pathakjainish@gmail.com>
2024-05-25 13:45:57 +0530

    Minor error resolved.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -19,7 +19,7 @@ function NavItems({ isModalView = false }) {
       }`}
       id="nav-items"
     >
-      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-gray-700">
+      <ul className={`flex flex-col p-4 md:p-0 mt-4 font-medium rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-gray-700 ${isModalView ? "" : "border border-gray-100"}`}>
         {isAdminView
           ? adminNavOptions.map((item) => (
               <li
