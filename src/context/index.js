fa34ff21bc74ce0a390e57c92a3fc334e3d84ffe
Jainish06 <pathakjainish@gmail.com>
2024-06-24 23:24:31 +0530

    Part of checkout page done.

--- src/context/index.js
+++ src/context/index.js
@@ -4,11 +4,20 @@ import { createContext, useEffect, useState } from "react";
 
 export const GlobalContext = createContext(null);
 
+const intialCheckoutFormData = {
+  shippingAddress: {},
+  paymentMethod: {},
+  totalPrice: 0,
+  isPaid: false,
+  paidAt: new Date(),
+  isProcessing: true,
+};
+
 export default function GlobalState({ children }) {
   const [showNavModal, setShowNavModal] = useState(false);
   const [isAuthUser, setIsAuthUser] = useState(false);
   const [user, setUser] = useState(false);
-  const [pageLevelLoader, setPageLevelLoader] = useState(true);
+  const [pageLevelLoader, setPageLevelLoader] = useState(false);
   const [componentLevelLoader, setComponentLevelLoader] = useState({
     loading: false,
     id: "",
@@ -24,13 +33,18 @@ export default function GlobalState({ children }) {
     country: "",
     postalCode: "",
   });
+  const [checkoutFormData, setCheckOutFormData] = useState(
+    intialCheckoutFormData
+  );
 
   useEffect(() => {
     console.log(Cookies.get("token"));
     if (Cookies.get("token") !== undefined) {
       setIsAuthUser(true);
       const userData = JSON.parse(localStorage.getItem("user")) || {};
+      const getcartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
       setUser(userData);
+      setCartItems(getcartItems);
     } else {
       setIsAuthUser(false);
     }
@@ -59,6 +73,8 @@ export default function GlobalState({ children }) {
         setAddress,
         addressFormData,
         setAddressFormData,
+        checkoutFormData,
+        setCheckOutFormData,
       }}
     >
       {children}
